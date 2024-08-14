c91e5687b17a5f20f1060748209502d0bfc3f165
jekovcar <jekovcar@gmail.com>
2024-08-14 20:32:50 +0300

    Create keyboard_layout.arabic.js
    Please to add arabic keyboart layout .

---
+++ Plugins/Input Plugins/Tango Keyboard/js/keyboard_layout.arabic.js
@@ -0,0 +1,48 @@
+accentKeyboard.layouts['arabic'] = [
+  { d: '۱', u: '1' }, //Row 1
+  { d: '۲', u: '2' },
+  { d: '۳', u: '3' },
+  { d: '٤', u: '4' },
+  { d: '۵', u: '5' },
+  { d: '٦', u: '6' },
+  { d: '۷', u: '7' },
+  { d: '۸', u: '8' },
+  { d: '۹', u: '9' },
+  { d: '٠', u: '0' },
+  { d: 'ص', u: ' ً' },  //Row 2
+  { d: 'ض', u: ' َ' },
+  { d: 'ق', u: ' ٌ' },
+  { d: 'ف', u: 'إ' },
+  { d: 'غ', u: '‘' },
+  { d: 'ع', u: '÷' },
+  { d: 'ه', u: '×' },
+  { d: 'خ', u: '؛' },
+  { d: 'ح', u: '>' },
+  { d: 'ج', u: '<' },
+  { d: 'ش', u: '"' },  //Row 3
+  { d: 'س', u: ':' },
+  { d: 'ي', u: '/' },
+  { d: 'ب', u: '،' }   
+  { d: 'ل', u: 'آ' },
+  { d: 'ا', u: 'ٍ'  },
+  { d: 'ت', u: ']' },
+  { d: 'ن', u: '[' },
+  { d: 'م', u: 'ل' },
+  {keyType:"DELETE"},
+  {keyType:"CAPS", css:'font-double'},//Row 4
+  { d: 'ظ', u: '~' },
+  { d: 'ط', u: 'ـ' },
+  { d: 'ذ', u: '’' },
+  { d: 'د', u: 'ا' },
+  { d: 'ز', u: '؟' },
+  { d: 'ر', u: 'ل' },
+  { d: 'و', u: '.' },
+  { d: 'ة', u: ',' },
+  { d: 'ث', u: ' ُ' },
+  {keyType:"ENTER", css:'key-span-2 font-double'}, //Row 5
+  {keyType:"SETTINGS"},
+  {keyType:"CLEAR_TEXT"},
+  { d: 'ك', u: 'أ' },
+  {keyType:"SPACE", css:'key-span-6 gray-key'},
+  {keyType:"SWITCH_LANG", css:'key-span-2'} 
+];
